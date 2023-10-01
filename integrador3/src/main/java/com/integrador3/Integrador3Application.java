93f38f6a4181c4cb64b074610fe810ba946a6a05
MarchioriRH <rubenmarchiori@gmail.com>
2023-09-30 23:08:31 -0300

    Now yes, it works

--- integrador3/src/main/java/com/integrador3/Integrador3Application.java
+++ integrador3/src/main/java/com/integrador3/Integrador3Application.java
@@ -2,13 +2,12 @@ package com.integrador3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-
-@Configuration
+@ComponentScan("com.integrador3")
 public class Integrador3Application {	
 	public static void main(String[] args) {
 		SpringApplication.run(Integrador3Application.class, args);
