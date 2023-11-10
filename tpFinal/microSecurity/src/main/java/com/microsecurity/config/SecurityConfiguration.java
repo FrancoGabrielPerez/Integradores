fc365943c150d86f6d63289f9d34f4db23bef46f
MarchioriRH <rubenmarchiori@gmail.com>
2023-11-10 19:56:08 -0300

    Security not working
    Co-authored-by: Franco Perez <FrancoGabrielPerez@users.noreply.github.com>
    Co-authored-by: luchomellu <luchomellu@users.noreply.github.com>

--- tpFinal/microSecurity/src/main/java/com/microsecurity/config/SecurityConfiguration.java
+++ tpFinal/microSecurity/src/main/java/com/microsecurity/config/SecurityConfiguration.java
@@ -1,67 +1,101 @@
 package com.microsecurity.config;
 
-import com.microsecurity.security.jwt.JwtConfigurer;
-import com.microsecurity.security.jwt.TokenProvider;
-import com.microsecurity.service.AuthorityConstant;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpMethod;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
+import com.microsecurity.security.jwt.JWTFilter;
+import com.microsecurity.security.jwt.JwtConfigurer;
+import com.microsecurity.security.jwt.TokenProvider;
+import com.microsecurity.service.AuthorityConstant;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.microsecurity.security.jwt.JWTFilter;
+import com.microsecurity.security.jwt.TokenProvider;
+import com.microsecurity.service.AuthorityConstant;
+
 @Configuration
-public class SecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    
+    @Autowired
+    private TokenProvider tokenProvider;
 
-    private final TokenProvider tokenProvider;
-
-
-    /**
-     * Password encoder
-     */
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain( HttpSecurity http ) throws Exception {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
         // AGREGAMOS NUESTRA CONFIG DE JWT.
-        // http
-        //     .apply( securityConfigurerAdapter() );
+        http.apply(jwtConfigurer());
+
         http
-            .csrf( AbstractHttpConfigurer::disable )
-            
-            // MANEJAMOS LOS PERMISOS A LOS ENDPOINTS.
-            .authorizeHttpRequests( auth -> auth
-                .requestMatchers("/monopatines").permitAll()
-                .requestMatchers("/api/register").permitAll()
-                .requestMatchers("/api/authenticate").permitAll()
-                //.requestMatchers("/api/prueba").hasAuthority(AuthorityConstant.ADMIN)
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/register").permitAll()
+                    .requestMatchers("/api/authenticate").permitAll()
+                    .requestMatchers("/api/prueba").hasAuthority(AuthorityConstant.ADMIN)
             )
-            .anonymous( AbstractHttpConfigurer::disable )
-            .sessionManagement( s -> s.sessionCreationPolicy( SessionCreationPolicy.STATELESS ) );
+            .anonymous(AbstractHttpConfigurer::disable)
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         http
-            .httpBasic( Customizer.withDefaults() );
-        return http.build();
+            .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .httpBasic(Customizer.withDefaults());
     }
 
-    /**
-     * Nuestra configuracion de JWT.
-     */
-    private JwtConfigurer securityConfigurerAdapter() {
+    @Bean
+    public JwtConfigurer jwtConfigurer() {
         return new JwtConfigurer(tokenProvider);
     }
+
+
+    @Bean
+    public JWTFilter jwtAuthenticationFilter() {
+        return new JWTFilter(tokenProvider);
+    }
 }
+
+
+    /*
+     @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // AGREGAMOS NUESTRA CONFIG DE JWT.
+        http.apply(jwtConfigurer());
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/register").permitAll()
+                    .requestMatchers("/api/authenticate").permitAll()
+                    .requestMatchers("/api/prueba").hasAuthority(AuthorityConstant.ADMIN)
+            )
+            .anonymous(AbstractHttpConfigurer::disable)
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+            .addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+            .httpBasic(Customizer.withDefaults());
+    }
+
+    @Bean
+    public JwtConfigurer jwtConfigurer() {
+        return new JwtConfigurer(tokenProvider);
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(tokenProvider);
+    }
+    */
+
